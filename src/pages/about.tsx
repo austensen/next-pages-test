d25dbb6af987869e1a70b9df7096977b2c09fedd
Maxwell Austensen <m.austensen@gmail.com>
2023-04-16 16:54:54 -0400

    add basic next setup

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header"
+
+export default function Home() {
+  return (
+    <>  
+    <Header />
+    <main>
+        <div>
+            <h1>About</h1>
+        </div>
+    </main>
+    </>
+  )
+}
